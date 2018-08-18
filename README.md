ed460e13d2e9fdaf9caa90f37e4d777c33308fbf
/dev/mull <mullagain@protonmail.com>
2018-08-17 20:58:21 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # garson
-# garson
-# garson
+A little http server.
+
+Its good for copying files from one device to another. I have used it for Cisco IOS upgrades,etc.
+
+
+Usage of ./garson:
+  -dir string
+        directory to serve (default "./")
+  -port int
+        optional port
+
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	green        = string([]byte{27, 91, 57, 55, 59, 52, 50, 109})
+	white        = string([]byte{27, 91, 57, 48, 59, 52, 55, 109})
+	yellow       = string([]byte{27, 91, 57, 55, 59, 52, 51, 109})
+	red          = string([]byte{27, 91, 57, 55, 59, 52, 49, 109})
+	blue         = string([]byte{27, 91, 57, 55, 59, 52, 52, 109})
+	magenta      = string([]byte{27, 91, 57, 55, 59, 52, 53, 109})
+	cyan         = string([]byte{27, 91, 57, 55, 59, 52, 54, 109})
+	reset        = string([]byte{27, 91, 48, 109})
+	disableColor = false
+)
+
+func main() {
+	port := flag.Int("port", 0, "optional port")
+	dir := flag.String("dir", "./", "directory to serve")
+	flag.Parse()
+	r := gin.New()
+	r.Use(Logger())
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		panic(err)
+	}
+	r.Use(static.Serve("/", static.LocalFile(*dir, true)))
+	fmt.Printf("\n%sVoila!%s Serving on %s%v%s. Dir %s%v%s\n", green, reset, blue, listener.Addr(), reset, blue, *dir, reset)
+
+	log.Fatal(http.Serve(listener, r))
+}
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Start timer
+		start := time.Now()
+		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
+
+		// Process request
+		c.Next()
+
+		// Log only when path is not being skipped
+		// Stop timer
+		end := time.Now()
+		latency := end.Sub(start)
+
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+		var statusColor, methodColor, resetColor string
+		statusColor = colorForStatus(statusCode)
+		methodColor = colorForMethod(method)
+		resetColor = reset
+		comment := c.Errors.ByType(gin.ErrorTypePrivate).String()
+
+		if raw != "" {
+			path = path + "?" + raw
+		}
+
+		fmt.Printf("%s[GARSON]%s %v |%s %3d %s| %13v | %15s |%s %-7s %s %s | %s\n%s",
+			green, reset, end.Format("2006/01/02 - 15:04:05"),
+			statusColor, statusCode, resetColor,
+			latency,
+			clientIP,
+			methodColor, method, resetColor,
+			path,
+			c.Request.Header.Get("User-Agent"),
+			comment,
+		)
+	}
+}
+
+func colorForStatus(code int) string {
+	switch {
+	case code >= http.StatusOK && code < http.StatusMultipleChoices:
+		return green
+	case code >= http.StatusMultipleChoices && code < http.StatusBadRequest:
+		return white
+	case code >= http.StatusBadRequest && code < http.StatusInternalServerError:
+		return yellow
+	default:
+		return red
+	}
+}
+
+func colorForMethod(method string) string {
+	switch method {
+	case "GET":
+		return blue
+	case "POST":
+		return cyan
+	case "PUT":
+		return yellow
+	case "DELETE":
+		return red
+	case "PATCH":
+		return green
+	case "HEAD":
+		return magenta
+	case "OPTIONS":
+		return white
+	default:
+		return reset
+	}
+}
